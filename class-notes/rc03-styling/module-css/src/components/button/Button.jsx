b013a4b28e348064b314f2c16f148f1230dd6680
tunahanyuce <yucetunahan.25@gmail.com>
2025-05-02 21:56:35 +0300

    g

---
+++ class-notes/rc03-styling/module-css/src/components/button/Button.jsx
@@ -0,0 +1,11 @@
+import "./Button.css";
+
+const Button = () => {
+  return (
+    <div className="wrapper">
+      <button className="btn btn-blue">Info</button>
+      <button className="btn btn-red">Back</button>
+    </div>
+  );
+};
+export default Button;
