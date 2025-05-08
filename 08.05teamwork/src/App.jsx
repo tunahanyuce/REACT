898fbfeeca874b0e0e26d034b47d8d40e82e56d9
tunahanyuce <yucetunahan.25@gmail.com>
2025-05-08 20:52:41 +0300

    s

---
+++ 08.05teamwork/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./companents/header/Header"
+import CardContainer from './'
+
+
+function App() {
+
+  return (
+    <>
+     <Header/>
+     <CardContainer/>
+
+
+    </>
+  )
+}
+
+export default App
--- class-notes/rc05-Events-states-classCompanent/src/App.jsx
+++ class-notes/rc05-Events-states-classCompanent/src/App.jsx
@@ -1,29 +1,26 @@
-import './App.css'
-import Events from './components/events/Events'
-import UseStateHook from './components/useStateHook/UseStateHook';
-import MoreData from './components/useStateHook/MoreData';
-import ClassComponent from './components/class-component/ClassComponent';
+import Events from "./components/events/Events";
+import UseStateHook from "./components/useStateHook/UseStateHook";
+import MoreData from "./components/useStateHook/MoreData";
+import ClassComponent from "./components/class-component/ClassComponent";
 
 function App() {
-
   // JS ALANI
-  const selamla=()=>{
-    alert("Merhaba")
-  }
+  const selamla = () => {
+    alert("Merhaba");
+  };
 
   return (
     <>
-    <div className='bg-warning text-center p-3'>
-    <h1> React Events</h1>
-    {/* <Events/> */}
-    {/* <UseStateHook/> */}
-    {/* <MoreData/>   */}
+      <div className="bg-warning text-center p-3">
+        <h1> React Events</h1>
+        {/* <Events/> */}
+        {/* <UseStateHook/> */}
+        {/* <MoreData/>   */}
 
-    <ClassComponent/>
-    </div>
-    
+        <ClassComponent />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
