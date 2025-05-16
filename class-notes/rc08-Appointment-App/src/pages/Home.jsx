7a67ea205f5cd944435533787764ae1f4b0215c6
tunahanyuce <yucetunahan.25@gmail.com>
2025-05-16 16:56:27 +0300

    s

--- class-notes/rc08-Appointment-App/src/pages/Home.jsx
+++ class-notes/rc08-Appointment-App/src/pages/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import {hastaData,doctorData} from "../helper/data"
+import { hastaData, doctorData } from "../helper/data";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  let [doctors, setDoctors] = useState(doctorData);
+  const [hastalar, setHastalar] = useState(hastaData);
 
-export default Home
+  return (
+    <div>
+      <div>
+        <header>
+          <h1>HOSPİTAL</h1>
+          <div className="dr">
+            {doctors.map((dr) => (
+              <div>
+                <img
+                  style={{ background: "aqua" }}
+                  src="{dr.doctorImg}"
+                  alt=""
+                  width="180px"
+                  height="150px"
+                />
+                <h4
+                  style={{ background: "aqua", borderLeft: "10px solid blue" }}
+                >
+                  {dr.doctorName}
+                </h4>
+              </div>
+            ))}
+          </div>
+        </header>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
