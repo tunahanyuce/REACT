899f915a66be8d6966d5649ded6b29654c49e04c
tunahanyuce <yucetunahan.25@gmail.com>
2025-05-21 20:57:53 +0300

    nn

---
+++ class-notes/rc10-databaseAxios/src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+// index.html e de bootstrap in js teşekkürünü script olarak ekledik
+
+function App() {
+  return (
+    <div>
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ class-notes/rc10-databaseAxios/src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
+const Home = () => {
+  const [tutorials, setTutorials] = useState([]);
+
+  const url = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+
+  //!GET (READ)
+
+  const getBilgiler = async () => {
+    const res = await axios.get(url);
+
+    console.log(res.data);
+
+    setTutorials(res.data);
+  };
+
+  useEffect(() => {
+    getBilgiler();
+  }, []);
+
+  return (
+    <div>
+      <BilgiList tutorials={tutorials} />
+    </div>
+  );
+};
+
+export default Home;
