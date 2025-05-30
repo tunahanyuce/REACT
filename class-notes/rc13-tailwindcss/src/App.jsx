e969713e4c2521a98e0ce2863bf0eb28271338f1
tunahanyuce <yucetunahan.25@gmail.com>
2025-05-30 21:03:30 +0300

    e

---
+++ class-notes/rc13-tailwindcss/src/App.jsx
@@ -0,0 +1,12 @@
+import Typography from "./components/Typography";
+
+
+function App() {
+  return (
+    <h1 className="text-3xl text-center text-green-700 font-bold underline">
+      <Typography/>
+    </h1>
+  );
+}
+
+export default App;
---
+++ class-notes/rc13-tailwindcss/src/components/Typography.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Typography = () => {
+  return (
+    <div>
+      {/*//Typography*/}
+      <p className="text-2xl">Typography</p>
+      <p className="text-sm">Typography</p>
+      <p className="text-base/8">Typography</p>
+      <p className="text-lg">Typography</p>
+      <p className="text-xl">Typography</p>
+      <p className="text-4xl">Başlık</p>
+      <p className="text-3xl">Başlık</p>
+
+    </div>
+  );
+};
+
+export default Typography;
